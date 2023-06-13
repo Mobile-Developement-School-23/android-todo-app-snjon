8934025257a1b371e1344f94e0170dc87bbe93e4
SnJon <snjon93@gmail.lcom>
2023-06-13 11:36:13 +0900

    change AppModule.kt

--- app/src/main/java/ru/yandex/school/todoapp/di/AppModule.kt
+++ app/src/main/java/ru/yandex/school/todoapp/di/AppModule.kt
@@ -6,10 +6,12 @@ import ru.yandex.school.todoapp.data.repository.TodoItemsRepositoryImpl
 import ru.yandex.school.todoapp.domain.repository.TodoItemsRepository
 import ru.yandex.school.todoapp.presentation.item.TodoItemViewModel
 import ru.yandex.school.todoapp.presentation.list.TodoListViewModel
+import ru.yandex.school.todoapp.presentation.navigation.AppNavigator
 
 val appModule = module {
 
-    viewModel { TodoItemViewModel(get()) }
-    viewModel { TodoListViewModel(get()) }
+    viewModel { (todoId: String) -> TodoItemViewModel(todoId, get()) }
+    viewModel { TodoListViewModel(get(), get()) }
+    single { AppNavigator() }
     single<TodoItemsRepository> { TodoItemsRepositoryImpl() }
 }
