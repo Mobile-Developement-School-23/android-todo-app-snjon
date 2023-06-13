54822f41adfdddb1f9ce5e0fd51cf14bc0e9278b
SnJon <snjon93@gmail.lcom>
2023-06-13 11:34:52 +0900

    update viewModels

--- app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemViewModel.kt
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemViewModel.kt
@@ -1,7 +1,14 @@
 package ru.yandex.school.todoapp.presentation.item
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import ru.yandex.school.todoapp.domain.repository.TodoItemsRepository
+import ru.yandex.school.todoapp.presentation.item.model.TodoItemScreenState
 
-class TodoItemViewModel(private val repository: TodoItemsRepository) : ViewModel() {
+class TodoItemViewModel(
+    private val todoItemId: String?,
+    private val repository: TodoItemsRepository
+) : ViewModel() {
+
+    val todoItemScreenState = MutableStateFlow(TodoItemScreenState(todoItemId))
 }
--- app/src/main/java/ru/yandex/school/todoapp/presentation/list/TodoListViewModel.kt
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/list/TodoListViewModel.kt
@@ -1,7 +1,84 @@
 package ru.yandex.school.todoapp.presentation.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.yandex.school.todoapp.domain.model.TodoItem
 import ru.yandex.school.todoapp.domain.repository.TodoItemsRepository
+import ru.yandex.school.todoapp.presentation.list.model.TodoListScreenState
+import ru.yandex.school.todoapp.presentation.navigation.AppNavigator
 
-class TodoListViewModel(private val repository: TodoItemsRepository) : ViewModel() {
+class TodoListViewModel(
+    private val repository: TodoItemsRepository,
+    private val navigator: AppNavigator
+) : ViewModel() {
+
+    val todoListItemsState = MutableStateFlow(TodoListScreenState.empty)
+
+    init {
+        refreshList()
+    }
+
+    private fun refreshList() {
+        viewModelScope.launch {
+            todoListItemsState.update { previousState ->
+                val completedCount: Int
+                val shouldShowCompleted = previousState.isCompletedShowed
+
+                val items = repository.getTodoItems()
+                    .value
+                    .map { it.value }
+                    .also { completedCount = it.filter { it.isCompleted }.size }
+                    .filter {
+                        if (shouldShowCompleted) {
+                            true
+                        } else {
+                            it.isCompleted.not()
+                        }
+                    }
+
+                TodoListScreenState(
+                    listItems = items,
+                    completedCount = completedCount,
+                    isCompletedShowed = previousState.isCompletedShowed
+                )
+            }
+        }
+    }
+
+    fun createNewTodo() {
+        navigator.openTodoItem()
+    }
+
+    fun openTodoItemInfo(todoItem: TodoItem) {
+        navigator.openTodoItem(todoItem)
+    }
+
+    fun checkTodoItem(todoItem: TodoItem) {
+        val checkedItem = todoItem.copy(isCompleted = todoItem.isCompleted.not())
+        viewModelScope.launch {
+            repository.saveTodoItem(checkedItem)
+            refreshList()
+        }
+    }
+
+    fun deleteTodoItem(todoItem: TodoItem) {
+        viewModelScope.launch {
+            repository.deleteTodoItem(todoItem)
+            refreshList()
+        }
+    }
+
+    fun changeCompletedTodosVisibility() {
+        todoListItemsState.update {
+            TodoListScreenState(
+                listItems = it.listItems,
+                completedCount = it.completedCount,
+                isCompletedShowed = it.isCompletedShowed.not()
+            )
+        }
+        refreshList()
+    }
 }
