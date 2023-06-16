f3119df2530900744ba43b719e2a3152ec043e6a
SnJon <snjon93@gmail.lcom>
2023-06-17 03:50:02 +0900

    update TodoItemFragment & viewModel

--- app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemFragment.kt
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemFragment.kt
@@ -1,15 +1,31 @@
 package ru.yandex.school.todoapp.presentation.item
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.switchmaterial.SwitchMaterial
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 import ru.yandex.school.todoapp.R
+import ru.yandex.school.todoapp.domain.model.TodoItemPriority
+import ru.yandex.school.todoapp.presentation.item.model.TodoItemScreenState
 import ru.yandex.school.todoapp.presentation.navigation.KEY_TODO_ITEM_ID
 import ru.yandex.school.todoapp.presentation.util.bind
+import ru.yandex.school.todoapp.presentation.util.convertFromLocalizedDateFormat
+import ru.yandex.school.todoapp.presentation.util.getCurrentDate
 import ru.yandex.school.todoapp.presentation.util.repeatOnCreated
+import ru.yandex.school.todoapp.presentation.util.setButtonColor
+import ru.yandex.school.todoapp.presentation.util.showDatePickerDialog
+import ru.yandex.school.todoapp.presentation.util.toLocalizedDate
+import ru.yandex.school.todoapp.presentation.util.visibleOrInvisible
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TodoItemFragment : Fragment(R.layout.fragment_todo_item) {
 
@@ -19,17 +35,132 @@ class TodoItemFragment : Fragment(R.layout.fragment_todo_item) {
         )
     }
 
-    private val screenText by bind<TextView>(R.id.todo_item_text)
+    private val editText by bind<TextView>(R.id.todo_item_text)
+    private val priority by bind<TextView>(R.id.todo_item_priority)
+    private val saveButton by bind<TextView>(R.id.todo_item_save)
+    private val deadlineDate by bind<TextView>(R.id.todo_item_date)
+    private val todoBeforeSwitch by bind<SwitchMaterial>(R.id.todo_item_switch)
+    private val deleteButton by bind<MaterialButton>(R.id.todo_item_delete_button)
+    private val toolbar by bind<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bindViews()
         subscribeOnViewModel()
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     private fun subscribeOnViewModel() {
         viewModel.todoItemScreenState.repeatOnCreated(this) {
-            screenText.text = "todo item id = ${it.todoItemId}"
+            showContent(it)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun showContent(content: TodoItemScreenState) {
+
+        editText.text = content.text
+        saveButton.isEnabled = editText.text.isNotEmpty()
+        deleteButton.isEnabled = editText.text.isNotEmpty()
+        deleteButton.setButtonColor(editText.text.isNotEmpty())
+
+        priority.text = getString(content.priorityRes)
+
+        content.deadlineDate?.let { date ->
+            deadlineDate.text = date.toLocalizedDate()
+            todoBeforeSwitch.isChecked = true
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun saveDate() {
+        val currentDate = getCurrentDate()
+        val deadlineDate = if (deadlineDate.text.toString().isNotEmpty()) {
+            deadlineDate.text.toString().convertFromLocalizedDateFormat()
+        } else {
+            null
+        }
+
+        viewModel.updateTodoItemDate(currentDate, deadlineDate)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun bindViews() {
+
+        saveButton.setOnClickListener {
+            saveDate()
+            viewModel.saveTodoItem()
+        }
+
+        editText.doOnTextChanged { text, _, _, _ ->
+            val newText = text.toString()
+            saveButton.isEnabled = newText.isNotEmpty()
+            deleteButton.isEnabled = newText.isNotEmpty()
+            deleteButton.setButtonColor(newText.isNotEmpty())
+            viewModel.updateTodoItemText(newText)
+        }
+
+        priority.setOnClickListener { view ->
+            val popupMenu = PopupMenu(view.context, view)
+            popupMenu.menuInflater.inflate(R.menu.priority_menu, popupMenu.menu)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                popupMenu.setForceShowIcon(true)
+            }
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_action_done -> {
+                        priority.text = menuItem.title
+                        viewModel.updateTodoItemPriority(TodoItemPriority.DEFAULT)
+                        true
+                    }
+
+                    R.id.menu_action_edit -> {
+                        priority.text = menuItem.title
+                        viewModel.updateTodoItemPriority(TodoItemPriority.LOW)
+                        true
+                    }
+
+                    R.id.menu_action_delete -> {
+                        priority.text = menuItem.title
+                        viewModel.updateTodoItemPriority(TodoItemPriority.HIGH)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+            popupMenu.show()
+        }
+
+        todoBeforeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                deadlineDate.visibleOrInvisible(true)
+                if (deadlineDate.text == "") {
+                    val currentDate = LocalDate.now()
+                    val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                    deadlineDate.text = currentDate.format(formatter).toLocalizedDate()
+                }
+            } else {
+                deadlineDate.visibleOrInvisible(false)
+                deadlineDate.text = ""
+            }
+        }
+
+        toolbar.setNavigationOnClickListener {
+            viewModel.closeTodoItem()
+        }
+
+        deadlineDate.setOnClickListener {
+            showDatePickerDialog(deadlineDate)
+        }
+
+        deleteButton.setOnClickListener {
+            viewModel.deleteTodoItem()
+            viewModel.closeTodoItem()
         }
     }
 }
--- app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemViewModel.kt
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/item/TodoItemViewModel.kt
@@ -2,13 +2,81 @@ package ru.yandex.school.todoapp.presentation.item
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import ru.yandex.school.todoapp.domain.model.TodoItem
+import ru.yandex.school.todoapp.domain.model.TodoItemPriority
 import ru.yandex.school.todoapp.domain.repository.TodoItemsRepository
 import ru.yandex.school.todoapp.presentation.item.model.TodoItemScreenState
+import ru.yandex.school.todoapp.presentation.navigation.AppNavigator
+import java.util.UUID
 
 class TodoItemViewModel(
     private val todoItemId: String?,
+    private val navigator: AppNavigator,
     private val repository: TodoItemsRepository
 ) : ViewModel() {
 
-    val todoItemScreenState = MutableStateFlow(TodoItemScreenState(todoItemId))
+    val todoItemScreenState = MutableStateFlow(TodoItemScreenState())
+
+    private var todoItem: TodoItem = loadTodoItem()
+
+    init {
+        loadContent()
+    }
+
+    private fun loadContent() {
+        todoItemScreenState.update {
+            TodoItemScreenState(
+                text = todoItem.text,
+                priorityRes = todoItem.priority.titleRes,
+                deadlineDate = todoItem.deadlineDate
+            )
+        }
+    }
+
+    fun updateTodoItemText(text: String) {
+        todoItem = todoItem.copy(text = text)
+    }
+
+    fun updateTodoItemPriority(priority: TodoItemPriority) {
+        todoItem = todoItem.copy(priority = priority)
+        todoItemScreenState.update { it.copy() }
+    }
+
+    fun updateTodoItemDate(currentDate: String, deadLineDate: String?) {
+        if (todoItem.createDate.isNullOrBlank()) {
+            todoItem = todoItem.copy(createDate = currentDate)
+        }
+
+        todoItem = todoItem.copy(
+            deadlineDate = deadLineDate,
+            changeDate = currentDate
+        )
+    }
+
+    private fun loadTodoItem(): TodoItem {
+        return if (todoItemId == null) {
+            TodoItem.empty
+        } else {
+            repository.getTodoById(todoItemId) ?: TodoItem.empty
+        }
+    }
+
+    fun saveTodoItem() {
+        repository.saveTodoItem(
+            todoItem.copy(
+                id = todoItemId ?: UUID.randomUUID().toString()
+            )
+        )
+        navigator.openTodoList()
+    }
+
+
+    fun closeTodoItem() {
+        navigator.openTodoList()
+    }
+
+    fun deleteTodoItem() {
+        repository.deleteTodoItem(todoItem)
+    }
 }
