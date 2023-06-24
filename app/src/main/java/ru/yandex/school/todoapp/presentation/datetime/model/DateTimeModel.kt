c3069c777e9688aa4366b6f1a67841d53a498e81
SnJon <snjon93@gmail.com>
2023-06-24 16:08:04 +0900

    add DateTimeModel

---
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/datetime/model/DateTimeModel.kt
@@ -0,0 +1,7 @@
+package ru.yandex.school.todoapp.presentation.datetime.model
+
+sealed class DateTimeModel {
+    class Date(val year: Int, val month: Int, val day: Int) : DateTimeModel()
+
+    class Time(val hourOfDay: Int, val minute: Int) : DateTimeModel()
+}
