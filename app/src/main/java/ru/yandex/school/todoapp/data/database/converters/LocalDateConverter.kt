4b61264b823ed91a29670aed0b755969d6c1390d
SnJon <napstertm@gmail.com>
2023-06-26 20:36:07 +0900

    add room TypeConverter

---
+++ app/src/main/java/ru/yandex/school/todoapp/data/database/converters/LocalDateConverter.kt
@@ -0,0 +1,31 @@
+package ru.yandex.school.todoapp.data.database.converters
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalDateConverter {
+
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @TypeConverter
+    fun fromDate(value: LocalDate?): String? {
+        return value?.toString()
+    }
+
+    @TypeConverter
+    fun toDate(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun fromDateTime(value: LocalDateTime?): String? {
+        return value?.format(formatter)
+    }
+
+    @TypeConverter
+    fun toDateTime(value: String?): LocalDateTime? {
+        return value?.let { LocalDateTime.parse(it, formatter) }
+    }
+}
