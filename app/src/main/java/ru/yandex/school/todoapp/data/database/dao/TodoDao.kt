ebff2597d67403623a499bb0194aa0b4d250109f
SnJon <napstertm@gmail.com>
2023-06-26 20:37:43 +0900

    add TodoDao

---
+++ app/src/main/java/ru/yandex/school/todoapp/data/database/dao/TodoDao.kt
@@ -0,0 +1,26 @@
+package ru.yandex.school.todoapp.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.yandex.school.todoapp.data.model.TodoEntity
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM TodoEntity ORDER BY modifiedAt DESC")
+    suspend fun getTodoItems(): List<TodoEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveTodoItem(todoItem: TodoEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveTodoItems(todos: List<TodoEntity>)
+
+    @Query("DELETE FROM TodoEntity WHERE id = :id")
+    suspend fun deleteTodoItem(id: String)
+
+    @Query("SELECT * FROM TodoEntity WHERE id = :id")
+    suspend fun getTodoById(id: String): TodoEntity?
+}
