13b34aa646e1ab25d7b763aa85d771c0584e1648
SnJon <napstertm@gmail.com>
2023-07-01 13:43:27 +0900

    change TodoItemsRepository: add updateTodoItem & loadFromServer

--- app/src/main/java/ru/yandex/school/todoapp/domain/repository/TodoItemsRepository.kt
+++ app/src/main/java/ru/yandex/school/todoapp/domain/repository/TodoItemsRepository.kt
@@ -9,9 +9,13 @@ interface TodoItemsRepository {
 
     suspend fun getTodoById(id: String): TodoItem?
 
-    suspend fun saveTodoItem(item: TodoItem)
+    suspend fun updateTodoItem(item: TodoItem)
+
+    suspend fun addTodoItem(item: TodoItem)
 
     suspend fun saveTodoItems(items: List<TodoItem>)
 
     suspend fun deleteTodoItem(item: TodoItem)
+
+    suspend fun loadFromServer()
 }
