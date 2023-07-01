84ce30ca825c50e6d45a31d361a3c8d3ee02a4ba
SnJon <napstertm@gmail.com>
2023-07-01 13:40:55 +0900

    change TodoDao: add getTodoItems & isUnsynchronized

--- app/src/main/java/ru/yandex/school/todoapp/data/database/dao/TodoDao.kt
+++ app/src/main/java/ru/yandex/school/todoapp/data/database/dao/TodoDao.kt
@@ -4,14 +4,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import ru.yandex.school.todoapp.data.model.TodoEntity
+import ru.yandex.school.todoapp.data.model.database.TodoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
 
     @Query("SELECT * FROM TodoEntity ORDER BY modifiedAt DESC")
-    fun getTodoItems(): Flow<List<TodoEntity>>
+    fun getAll(): Flow<List<TodoEntity>>
+
+    @Query("SELECT * FROM TodoEntity ORDER BY modifiedAt DESC")
+    fun getTodoItems(): List<TodoEntity>
+
+    @Query("SELECT EXISTS(SELECT 1 FROM TodoEntity WHERE isSync = 1 LIMIT 1)")
+    fun isUnsynchronized(): Boolean
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveTodoItem(todoItem: TodoEntity)
