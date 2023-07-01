8da03f9c8144ff9c0d6b3f75ea4f8cbb1a57b105
SnJon <napstertm@gmail.com>
2023-07-01 13:34:42 +0900

    add OkHttpProvider

---
+++ app/src/main/java/ru/yandex/school/todoapp/data/provider/OkHttpProvider.kt
@@ -0,0 +1,20 @@
+package ru.yandex.school.todoapp.data.provider
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import ru.yandex.school.todoapp.BuildConfig
+import ru.yandex.school.todoapp.data.api.interceptor.AuthInterceptor
+
+class OkHttpProvider(private val authInterceptor: AuthInterceptor) {
+
+    fun provide(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        }
+
+        return builder.build()
+    }
+}
