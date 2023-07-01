84291f19b21aa1cdaf13576e6b5eb7fa84a92655
SnJon <napstertm@gmail.com>
2023-07-01 13:48:41 +0900

    update TodoItemViewModel

--- app/src/main/java/ru/yandex/school/todoapp/presentation/item/viewmodel/TodoItemViewModel.kt
+++ app/src/main/java/ru/yandex/school/todoapp/presentation/item/viewmodel/TodoItemViewModel.kt
@@ -1,18 +1,28 @@
 package ru.yandex.school.todoapp.presentation.item.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import ru.yandex.school.todoapp.data.model.error.ApiError
+import ru.yandex.school.todoapp.data.model.error.DbError
+import ru.yandex.school.todoapp.data.model.error.NetworkError
+import ru.yandex.school.todoapp.data.model.error.UnknownHostException
 import ru.yandex.school.todoapp.domain.model.TodoItem
 import ru.yandex.school.todoapp.domain.model.TodoItemPriority
 import ru.yandex.school.todoapp.domain.repository.TodoItemsRepository
+import ru.yandex.school.todoapp.presentation.base.BaseViewModel
 import ru.yandex.school.todoapp.presentation.datetime.model.DateTimeModel
 import ru.yandex.school.todoapp.presentation.item.model.TodoItemScreenState
 import ru.yandex.school.todoapp.presentation.item.viewmodel.mapper.TodoItemDateMapper
 import ru.yandex.school.todoapp.presentation.navigation.AppNavigator
+import ru.yandex.school.todoapp.presentation.util.SingleLiveEvent
 import ru.yandex.school.todoapp.presentation.util.toDate
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -23,9 +33,15 @@ class TodoItemViewModel(
     private val navigator: AppNavigator,
     private val repository: TodoItemsRepository,
     private val dateMapper: TodoItemDateMapper
-) : ViewModel() {
+) : BaseViewModel() {
 
     val todoItemScreenState = MutableStateFlow(TodoItemScreenState())
+
+    private val _todoUpdatedLiveData = SingleLiveEvent<Boolean>()
+    val todoUpdatedLiveData = _todoUpdatedLiveData
+
+    private val _errorLiveData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String> = _errorLiveData
 
     private var todoItem: TodoItem = runBlocking { loadTodoItem() }
 
@@ -39,7 +55,7 @@ class TodoItemViewModel(
                 text = todoItem.text,
                 priorityRes = todoItem.priority.titleRes,
                 deadlineDate = todoItem.deadline?.let { dateMapper.map(it) },
-                createDate = todoItem.createAt?.let { dateMapper.map(it) }
+                modifiedDate = todoItem.modifiedAt?.let { dateMapper.map(it) }
             )
         }
     }
@@ -82,31 +98,69 @@ class TodoItemViewModel(
         }
     }
 
-    fun saveTodoItem() {
+    fun addTodoItem() {
         val isCreating = todoItemId == null
         val currentDate = LocalDate.now()
         val currentDateTime = LocalDateTime.now()
 
-        viewModelScope.launch {
-            repository.saveTodoItem(
-                todoItem.copy(
-                    id = todoItemId ?: UUID.randomUUID().toString(),
-                    createAt = if (isCreating) currentDate else todoItem.createAt,
-                    modifiedAt = currentDateTime
+        val upsertJob = launchJob(
+            onError = { handleAppError(it) }
+        )
+        {
+            if (isCreating) {
+                Log.e("todo", "creating")
+                repository.addTodoItem(
+                    todoItem.copy(
+                        id = UUID.randomUUID().toString(),
+                        createAt = currentDate,
+                        modifiedAt = currentDateTime
+                    )
                 )
-            )
+            } else {
+                Log.e("todo", "changing")
+                repository.updateTodoItem(
+                    todoItem.copy(
+                        modifiedAt = currentDateTime
+                    )
+                )
+            }
         }
-        closeTodoItem()
+        onWaitCompleteJob(upsertJob)
     }
 
+    // Жёсткий костыль! Буду рад, если поможете исправить
+    // Проблема в том, что если вовзращаемся на TodoListFragment без ожидания, то запрос не успевает выполниться
+    // Если делаем в репозитории отдельную Job с join(), тогда падает приложение в случае Exception (например при отсутствии интернета)
+    private fun onWaitCompleteJob(job: Job) {
+        viewModelScope.launch {
+            while (true) {
+                delay(200)
+                _todoUpdatedLiveData.postValue(job.isCompleted)
+            }
+        }
+    }
 
     fun closeTodoItem() {
-        navigator.openTodoList()
+        navigator.backTodoList()
     }
 
     fun deleteTodoItem() {
-        viewModelScope.launch {
+        launchJob(
+            onError = { }
+        ) {
             repository.deleteTodoItem(todoItem)
         }
+    }
+
+    private fun handleAppError(error: Throwable) {
+        val errorMessage = when (error) {
+            is NetworkError -> "Ошибка сети"
+            is UnknownHostException -> "Ошибка сети"
+            is DbError -> "Ошибка базы данных"
+            is ApiError -> "Ошибка API: ${error.status} ${error.code}"
+            else -> "Отсутствует соединение с интернетом"
+        }
+
+        _errorLiveData.postValue(errorMessage)
     }
 }
