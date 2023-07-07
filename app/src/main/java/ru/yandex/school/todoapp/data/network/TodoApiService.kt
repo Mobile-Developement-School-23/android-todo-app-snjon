157f4b52da7b170c8f4140c693a5d3a65999bf21
SnJon <snjon93@gmail.lcom>
2023-07-07 23:15:35 +0900

    update import statements for retrofit annotations

--- app/src/main/java/ru/yandex/school/todoapp/data/network/TodoApiService.kt
+++ app/src/main/java/ru/yandex/school/todoapp/data/network/TodoApiService.kt
@@ -1,7 +1,14 @@
 package ru.yandex.school.todoapp.data.network
 
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import ru.yandex.school.todoapp.data.model.request.AddTodoListRequest
 import ru.yandex.school.todoapp.data.model.request.AddTodoRequest
 import ru.yandex.school.todoapp.data.model.response.TodoItemResponse
